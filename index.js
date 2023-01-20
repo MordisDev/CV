80fd4dc2fb0baaa6bfa0e411dd44d741277b1bef
Mordis <mordis@Mordiss-MacBook-Pro.local>
2023-01-20 18:50:26 +0100

    Working CV site

---
+++ index.js
@@ -0,0 +1,193 @@
+// CREATE HELPER FUNCTIONS
+
+function generateElement(type, className, content) {
+    const Element = document.createElement(type);
+    if (className) Element.setAttribute('class', className);
+    if (content) Element.textContent = content;
+
+    return Element;
+}
+
+function generateSectioWithTitle(title) {
+    const Section = generateElement('section');
+    Section.appendChild(generateElement('h5','section_title', title));
+
+    return Section;
+}
+
+function generateListItemWithIcon(icon, content) {
+    const Item = document.createElement('li');
+    
+    const Icon = document.createElement('i');
+    Icon.setAttribute('class', icon);
+    Item.appendChild(Icon);
+
+    const Content = document.createElement('span');
+    Content.setAttribute('class', 'description');
+    Content.textContent = content;
+    Item.appendChild(Content);
+
+    return Item;
+}
+
+function generateListSection(title, className, listItems) {
+    
+    const Section = generateSectioWithTitle(title);
+    const List = generateElement('ul', className);
+
+    listItems.map(item => {
+        const Item = generateElement('li');
+        Item.innerHTML = item;
+        List.appendChild(Item);
+    });
+
+    Section.appendChild(List);
+
+    return Section;
+}
+
+// PERSONAL INFORMATION SECTION
+
+const { firstName, lastName, workTitle, contactAndLinks } = sourceData;
+
+const Header = generateElement('section', 'header');
+
+const PersonalInformation = generateElement('div', 'personal_info');
+
+PersonalInformation.appendChild(generateElement('h2', 'name', `${firstName} ${lastName}`));
+PersonalInformation.appendChild(generateElement('h3', 'title', workTitle));
+
+const LinksAndContact = generateElement('ul','links_and_contact');
+contactAndLinks.map(item =>
+    LinksAndContact.appendChild(
+        generateListItemWithIcon(item.icon, item.content)
+    ));
+
+PersonalInformation.appendChild(LinksAndContact);
+Header.appendChild(PersonalInformation);
+
+const Picture = generateElement('img');
+Picture.src = './img/IMG_0257.jpeg';
+
+Header.appendChild(Picture);
+
+
+// MAIN LAYOUT
+
+const MainLayoutWrapper = generateElement('div', 'main_layout');
+
+const LeftColumnWrapper = generateElement('div', 'left_column');
+MainLayoutWrapper.appendChild(LeftColumnWrapper);
+
+const RightColumnWrapper = generateElement('div', 'right_column');
+MainLayoutWrapper.appendChild(RightColumnWrapper);
+
+// SUMMARY
+
+const { summary } = sourceData;
+
+const Summary = generateSectioWithTitle("Summary");
+
+const SummaryContent = generateElement('p', 'summary');
+SummaryContent.innerHTML = summary;
+Summary.appendChild(SummaryContent);
+
+LeftColumnWrapper.appendChild(Summary);
+
+// EDUCATION
+
+const { education } = sourceData;
+
+const Education = generateSectioWithTitle("Education");
+
+education.map(educationItem => {
+
+    const { start, end, title, school, speciality } = educationItem;
+    
+    const Item = generateElement('div', 'education_item');
+    Item.appendChild(generateElement('h5', 'title', title));
+
+    const SpecializationAndSchool = generateElement('h6', 'speciality_and_school');
+    SpecializationAndSchool.innerHTML = `${speciality}, <br /> at ${school}`;
+    Item.appendChild(SpecializationAndSchool);
+    
+    const Time = generateElement('ul', 'time_and_location');
+    Time.appendChild(generateListItemWithIcon('fa-regular fa-calendar-days', `${start} - ${end}`));
+    Item.appendChild(Time);
+
+    Education.appendChild(Item);
+});
+
+LeftColumnWrapper.appendChild(Education);
+
+// WORK EXPERIENCE
+
+const { workHistory } = sourceData;
+
+const WorkExperience = generateSectioWithTitle("Experience");
+
+workHistory.map(workHistoryItem => {
+
+    const { start, end, title, company, location, description } = workHistoryItem;
+    
+    const Item = generateElement('div', 'work_experience_item');
+    Item.appendChild(generateElement('h6','position', title));
+    Item.appendChild(generateElement('div','company_name', company));
+
+    const TimeAndLocation = generateElement('ul', 'time_and_location');
+    TimeAndLocation.appendChild(generateListItemWithIcon('fa-regular fa-calendar-days', `${start} - ${end}`));
+    TimeAndLocation.appendChild(generateListItemWithIcon('fa-solid fa-location-dot', location));
+    
+    Item.appendChild(TimeAndLocation);
+
+    const JobDescription = generateElement('ul', 'description');
+    description.map(descriptionItem => JobDescription.appendChild(generateElement('li', null, descriptionItem)));
+    Item.appendChild(JobDescription)
+
+    WorkExperience.appendChild(Item);
+});
+
+LeftColumnWrapper.appendChild(WorkExperience);
+
+// SKILLS
+
+const { softSkills } = sourceData;
+
+RightColumnWrapper.appendChild(generateListSection("Skills", 'soft_skills', softSkills));
+
+// TECH STACK
+
+const { techStack } = sourceData;
+
+RightColumnWrapper.appendChild(generateListSection("Tech Stack", 'tech_stack', techStack));
+
+// LANGUAGES
+
+const { languages } = sourceData;
+
+const Languages = generateSectioWithTitle("Languages");
+
+languages.map(item => {
+    const Language = generateElement('div', 'language');
+    Language.innerHTML = `${item.language}: <span>${item.proficiency}</span>`;
+    Languages.appendChild(Language);
+});
+
+RightColumnWrapper.appendChild(Languages);
+
+// PERSONAL INTERESTS
+
+const { interests } = sourceData;
+
+RightColumnWrapper.appendChild(generateListSection("Interests", 'interests', interests));
+
+// LEGAL
+
+const { legalNotice } = sourceData;
+
+const LegalNotice = generateElement('p', 'legal', legalNotice);
+
+const cv_body = document.getElementById('cv_body');
+cv_body.appendChild(Header);
+cv_body.appendChild(MainLayoutWrapper);
+cv_body.appendChild(LegalNotice);
---
+++ sourceData.js
@@ -0,0 +1,153 @@
+const sourceData = {
+    firstName: "Ireneusz",
+    lastName: "Rybiński",
+    workTitle: "Front End Developer",
+    contactAndLinks: [
+        {
+            icon: "fa-solid fa-phone",
+            content: "+48 503 489 525",
+        },
+        {
+            icon: "fa-solid fa-at",
+            content: "ireneusz.rybinski@gmail.com",
+        },
+        {
+            icon: "fa-solid fa-cake-candles",
+            content: "1984 - 09 - 02",
+        },
+        {
+            icon: "fa-solid fa-location-dot",
+            content: "Gdańsk, PL",
+        },
+        {
+            icon: "fa-brands fa-github",
+            content: "github.com/mordisdev",
+        },
+        {
+            icon: "fa-brands fa-linkedin",
+            content: "linkedin.com/in/irek-rybinski-85734384",
+        },
+    ],
+    summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur lobortis sodales felis eu imperdiet. Quisque convallis congue erat, rutrum eleifend mi consectetur eget. Fusce iaculis porta ante, eget hendrerit lorem fermentum vel. Nullam massa mi, ullamcorper quis ligula ac, luctus condimentum libero. Sed aliquet feugiat est ac tempus. Proin malesuada ante non justo accumsan luctus. Vestibulum ultricies sed lorem ut egestas. Nunc varius, tortor ornare convallis scelerisque, velit nisi vestibulum diam, sit amet vehicula lectus diam ultricies arcu. Mauris eget.",
+    workHistory: [
+        {
+            "start": "02.2019",
+            "end": "present",
+            "title": "Front-end Dev / Deliverability Engineer",
+            "company": "GetResponse S.A.",
+            "location": "Gdańsk",
+            "description": [
+                "Design and preparation of various functional and analytical tools in form of team specific work panel (with core technologies of Laravel for backend, and React, Typescript, Styled Components for frontend)",
+                "Assisting with various deliverability related projects",
+            ],
+        },
+        {
+            "start": "05.2016",
+            "end": "02.2019",
+            "title": "Deliverability Engineer",
+            "company": "GetResponse S.A.",
+            "location": "Gdańsk",
+            "description": [
+                "Configuration and support of mass mailing sending structure on application and server side (MySQL, PMTA)",
+                "Keeping sending structure up to date with current technical standards",
+                "Preparing and implementing projects in area of Email deliverability",
+                "Consulting, advising and mentoring for other team members",
+            ],
+        },
+        {
+            "start": "12.2014",
+            "end": "05.2016",
+            "title": "Deliverability Manager",
+            "company": "GetResponse S.A.",
+            "location": "Gdańsk",
+            "description": [
+                "Maintenance and development of mass sending infrastructure on application and server side",
+                "Building and maintaining working relationship with Email Service Providers",
+                "Working with key customers on improving their marketing campaign results",
+                "Preparing and implementing projects in area of Email deliverability",
+                "Recruiting, consulting, advising and mentoring for other team members",
+            ],
+        },
+        {
+            "start": "04.2013",
+            "end": "12.2014",
+            "title": "Privacy and Deliverability Manager",
+            "company": "GetResponse S.A.",
+            "location": "Gdańsk",
+            "description": [
+                "Identification and handling of customers abusing the platform",
+                "Maintenance and development of mass sending infrastructure on application and server side",
+                "Building and maintaining working relationship with Email Service Providers",
+                "Working with key customers on improving their marketing campaign results",
+                "Preparing and implementing projects in area of Email deliverability",
+                "Recruiting, consulting, advising and mentoring for other team members",
+            ],
+        },
+        {
+            "start": "01.2011",
+            "end": "04.2013",
+            "title": "Senior Account Manager",
+            "company": "GetResponse S.A.",
+            "location": "Gdańsk",
+            "description": [
+                "Working with key customers to accommodate their marketing needs",
+                "Providing email marketing strategy advices",
+                "Fine tuning technical aspect of sending infrastructure",
+                "Mediating between the customer and Email Service Providers to mitigate delivery issues",
+            ],
+        },
+        {
+            "start": "03.2010",
+            "end": "01.2011",
+            "title": "Junior Data Analyst",
+            "company": "Voivodeship Labour Office",
+            "location": "Gdańsk",
+            "description": [
+                "Collection of job market related data",
+                "Assisting in preparation of various reports for government entities",
+            ],
+        },
+    ],
+    education: [
+        {
+            start: "10.2003",
+            end: "12.2009",
+            title: "Master in field of Mathematics",
+            speciality: "Faculty of Mathematics, Physics and Informatics",
+            school: "University of Gdansk",
+        }
+    ],
+    softSkills: [
+        "Good knowledge of Agile methodology",
+        "Practical knowledge of CI/CD, GIT, JIRA",
+        "Writing specifications and documentation",
+        "Good communication skills, technical and non-technical"
+    ],
+    techStack: [
+        "HTML5/CSS3",
+        "JavaScript",
+        "TypeScript",
+        "React.js",
+        "Styled Components",
+        "Css-in-JS",
+        "SQL (MySQL)",
+        "PHP",
+    ],
+    languages: [
+        {
+            language: "Polish",
+            proficiency: "Native",
+        },
+        {
+            language: "English",
+            proficiency: "Proficient",
+        }
+    ],
+    interests: [
+        "Carpentry (most furniture in my house is self made)",
+        "Lego Technic (the bigger set the better)",
+        "Games and Movies (for some time now I’m fascinated by technical aspects in those media, like story telling, character development, world creating algorithms in games, etc.)",
+        "Programming (I’m currently working on a simple app to help me organise myself in future projects, and slowly preparing myself to write my own web based game)"
+    ],
+    legalNotice: "I hereby give consent for my personal data included in the application to be processed for the purposes of the recruitment process in accordance with Art. 6 paragraph 1 letter a of the Regulation of the European Parliament and of the Council (EU) 2016/679 of 27 April 2016 on the protection of natural persons with regard to the processing of personal data and on the free movement of such data, and repealing Directive 95/46/EC (General Data Protection Regulation)."
+}
